8e4eb1b884f6f39e82942632d89d63553f855f99
unknown <venk01atesh@gmail.com>
2026-01-08 22:42:29 +0530

    F

--- array_practise.java
+++ array_practise.java
@@ -1,5 +1,11 @@
 class array_practise{
     public static void main(String[] args) {
+         int[] array_method=m1();
+        for(int i=0;i<array_method.length;i++){
+            System.out.println(array_method[i]);
+        }
+          int[] arr={1,2,3,4,5};
+        sum(arr);
         Student[] array;
         array=new Student[5];
         array[0]=new Student("Alice",1);
@@ -10,8 +16,20 @@ class array_practise{
         for(int i=0;i<array.length;i++){
             System.out.println("Name: "+array[i].name+" Roll No: "+array[i].rollno);
         }
-
+       
+     
     }
+    
+public static void sum(int[] arr){
+    int add=0;
+    for(int i=0;i<arr.length;i++)
+        add=add+arr[i];
+    System.out.println(add);
+
+}
+public static int[] m1(){
+    return new int[] {1,2,3,4,5};
+}
 }
 class Student{
     String name;
